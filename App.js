32a227d0122f239645f0a4d10b3867bc7c9bb10a
cafeman883 <sajukiller@gmail.com>
2020-04-10 22:51:27 +0900

    commit

--- App.js
+++ App.js
@@ -1,38 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableWithoutFeedback } from 'react-native';
+import Loading from "./Loading.js";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.yellowView}>
-        
-      </View>
-      <View style={styles.blueView}>
-        
-      </View>
-    </View>
-  );
+  return <Loading />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    //alignItems: 'center',
-    justifyContent: 'center'
-  },
-  text: {
-    fontSize: 72
-  },
-  yellowView: {
-    flex: 1, // flex로 레이아웃 잡는걸 추천한다. 비율임.
-    backgroundColor: "yellow"
-  },
-  blueView: {
-    flex: 2,
-    backgroundColor: "blue"
-  }
-  
-});
  
 
 
