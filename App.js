1e1102a6396a565d7f79806527b8bcc83f1b968b
cafeman883 <sajukiller@gmail.com>
2020-04-11 07:52:54 +0900

    commit

--- App.js
+++ App.js
@@ -1,8 +1,38 @@
 import React from 'react';
+import {Alert} from "react-native";
 import Loading from "./Loading.js";
+import * as Location from 'expo-location';
 
-export default function App() {
-  return <Loading />;
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  }
+
+  getLocation = async() => {
+    try {
+      //throw Error(); 임의로 에러 발생시켜 보기/
+      await Location.requestPermissionsAsync();
+      const {
+        coords: {latitude, longitude}
+      } = await Location.getCurrentPositionAsync();
+
+      this.setState({ isLoading: false});
+
+      // Send to API and get weather!
+    } catch (error) {
+      Alert.alert("Can't find you.", "So Sad");
+    }
+
+    
+  }
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
+  } 
 }
  
 
